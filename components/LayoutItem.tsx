835aa0905b018021bc9d5284055bebc914ff6da8
Felice <felicepng2010@gmail.com>
2022-02-28 19:49:58 +0800

    Edit UI

--- components/LayoutItem.tsx
+++ components/LayoutItem.tsx
@@ -1,8 +1,9 @@
 import { useRouter } from "next/router";
+import { ReactElement } from "react";
 
 interface Props {
   title: string
-  icon: any
+  icon: ReactElement
 }
 
 function LayoutItem(props: Props) {
@@ -11,7 +12,7 @@ function LayoutItem(props: Props) {
 
   return (
     <button onClick={() => router.push(`/${props.title.toLowerCase()}`)}
-      className={`w-full px-10 flex items-center py-5 text-base gap-5 hover:bg-gray-100 text-gray-700 hover:text-blue-600 border-l-4 border-transparent hover:font-poppins-semibold hover:border-blue-600 ${focused && "border-l-4 bg-gray-100 text-blue-600 border-blue-600 font-poppins-semibold"}`}
+      className={`w-full px-10 flex items-center py-5 text-base gap-5 hover:bg-gray-100 hover:text-blue-600 border-l-4 border-transparent hover:font-poppins-semibold hover:border-blue-600 ${focused && "border-l-4 bg-gray-100 text-blue-600 border-blue-600 font-poppins-semibold"}`}
     >
       {props.icon}
       <div className="text-sm">{props.title}</div>
