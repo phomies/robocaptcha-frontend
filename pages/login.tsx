4e7947d3e5a0642abf85b025408272310b056043
Felice <felicepng2010@gmail.com>
2022-03-01 08:16:16 +0800

    Edit login and register screens

--- pages/login.tsx
+++ pages/login.tsx
@@ -12,19 +12,19 @@ export default function Login() {
         <div className="self-center -mt-60">
           <h1 className="font-poppins-semibold text-secondary text-5xl mb-6">Sign in to</h1>
           <h1 className="font-poppins-medium text-secondary text-3xl mb-12">our user platform</h1>
-          <h1 className="font-poppins-regular text-sm mb-1">If you don't have an account registered <br /> You can register <a className="font-poppins-semibold text-blue-600 hover:underline" href="/register">here!</a></h1>
+          <h1 className="font-poppins-regular text-sm mb-1">If you don't have an account registered, <br /> You can register <a className="font-poppins-semibold text-blue-600 hover:underline" href="/register">here!</a></h1>
         </div>
 
         <img className="w-7/12 mt-48" alt="login" src="/images/login.png" />
 
-        <div className="justify-end self-center -mt-44">
+        <div className="text-sm text-blue-darkBlue font-poppins-regular justify-end self-center -mt-44">
           <form>
-            <h1 className="flex font-poppins-semibold text-2xl mb-9">Sign in</h1>
-            <input className="placeholder:text-blue-darkBlue focus:outline-none font-poppins-regular text-sm text-blue-darkBlue px-5 w-10/12 h-14 rounded-lg bg-blue-lightBlue mb-10"
+            <h1 className="text-black flex font-poppins-semibold text-2xl mb-9">Sign in</h1>
+            <input className="placeholder:text-blue-darkBlue focus:outline-none px-5 w-10/12 h-14 rounded-lg bg-blue-lightBlue mb-10"
               placeholder="Email or username"
               type="email"
             />
-            <input className="font-poppins-regular placeholder:text-blue-darkBlue focus:outline-none text-sm text-blue-darkBlue px-5 w-10/12 h-14 rounded-lg bg-blue-lightBlue mb-11"
+            <input className="placeholder:text-blue-darkBlue focus:outline-none px-5 w-10/12 h-14 rounded-lg bg-blue-lightBlue mb-11"
               placeholder="Password"
               type="password"
             />
--- pages/register.tsx
+++ pages/register.tsx
@@ -12,32 +12,32 @@ export default function Register() {
         <div className="self-center -mt-60">
           <h1 className="font-poppins-semibold text-secondary text-5xl mb-6">Sign up for</h1>
           <h1 className="font-poppins-medium text-secondary text-3xl mb-12">our user platform</h1>
-          <h1 className="font-poppins-regular text-sm mb-1">If you already have an account <br /> You can login <a className="font-poppins-semibold text-blue-600 hover:underline" href="/login">here!</a></h1>
+          <h1 className="font-poppins-regular text-sm mb-1">If you already have an account, <br /> You can login <a className="font-poppins-semibold text-blue-600 hover:underline" href="/login">here!</a></h1>
         </div>
 
         <img className="w-7/12 mt-48" alt="login" src="/images/login.png" />
 
-        <div className="justify-end self-center -mt-40">
+        <div className="font-poppins-regular text-blue-darkBlue text-sm justify-end self-center -mt-40">
           <form>
-            <h1 className="flex font-poppins-semibold text-2xl mb-7">Sign Up</h1>
-            <input className="placeholder:text-blue-darkBlue focus:outline-none font-poppins-regular text-sm text-blue-darkBlue px-5 w-10/12 h-14 rounded-lg bg-blue-lightBlue mb-3"
+            <h1 className="text-black flex font-poppins-semibold text-2xl mb-7">Sign Up</h1>
+            <input className="placeholder:text-blue-darkBlue focus:outline-none px-5 w-10/12 h-14 rounded-lg bg-blue-lightBlue mb-3"
               placeholder="Name"
             />
-            <input className="placeholder:text-blue-darkBlue focus:outline-none font-poppins-regular text-sm text-blue-darkBlue px-5 w-10/12 h-14 rounded-lg bg-blue-lightBlue mb-3"
+            <input className="placeholder:text-blue-darkBlue focus:outline-none px-5 w-10/12 h-14 rounded-lg bg-blue-lightBlue mb-3"
               placeholder="Username"
             />
-            <input className="placeholder:text-blue-darkBlue focus:outline-none font-poppins-regular text-sm text-blue-darkBlue px-5 w-10/12 h-14 rounded-lg bg-blue-lightBlue mb-3"
+            <input className="placeholder:text-blue-darkBlue focus:outline-none px-5 w-10/12 h-14 rounded-lg bg-blue-lightBlue mb-3"
               placeholder="Email"
               type="email"
             />
-            <input className="font-poppins-regular placeholder:text-blue-darkBlue focus:outline-none text-sm text-blue-darkBlue px-5 w-10/12 h-14 rounded-lg bg-blue-lightBlue mb-3"
+            <input className="placeholder:text-blue-darkBlue focus:outline-none px-5 w-10/12 h-14 rounded-lg bg-blue-lightBlue mb-3"
               placeholder="Phone number"
             />
-            <input className="font-poppins-regular placeholder:text-blue-darkBlue focus:outline-none text-sm text-blue-darkBlue px-5 w-10/12 h-14 rounded-lg bg-blue-lightBlue mb-3"
+            <input className="placeholder:text-blue-darkBlue focus:outline-none px-5 w-10/12 h-14 rounded-lg bg-blue-lightBlue mb-3"
               placeholder="Password"
               type="password"
             />
-            <input className="font-poppins-regular placeholder:text-blue-darkBlue focus:outline-none text-sm text-blue-darkBlue px-5 w-10/12 h-14 rounded-lg bg-blue-lightBlue mb-7"
+            <input className="placeholder:text-blue-darkBlue focus:outline-none px-5 w-10/12 h-14 rounded-lg bg-blue-lightBlue mb-7"
               placeholder="Confirm password"
               type="password"
             />
