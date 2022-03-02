7f299915212a1ea5e66bcf0edf3ab8a3fffa52f6
yvonnelhs <yvonnefghijk@gmail.com>
2022-03-02 13:00:53 +0800

    log in responsive

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,30 +6,37 @@ export default function Login() {
   return (
     <div className="flex flex-col h-screen overflow-y-hidden">
       <nav className="h-12">
-        <img className="h-8 my-8 mx-32" alt="icon" src="/logo.png" />
+        <img className="h-8 my-8 sm:mx-32 mx-10" alt="icon" src="/logo.png" />
       </nav>
-      <div className="grid grid-cols-3 mx-32 h-full">
-        <div className="self-center -mt-60">
-          <h1 className="font-poppins-semibold text-secondary text-5xl mb-6">Sign in to</h1>
-          <h1 className="font-poppins-medium text-secondary text-3xl mb-12">our user platform</h1>
-          <h1 className="font-poppins-regular text-sm mb-1">If you don't have an account registered, <br /> You can register <a className="font-poppins-semibold text-blue-600 hover:underline" href="/register">here!</a></h1>
+      <div className="grid lg:grid-cols-2 xl:grid-cols-3 sm:mx-32 mx-10 h-full">
+        <div className="self-center sm:mt-10 lg:-mt-24 xl:-mt-44 ">
+          <h1 className="font-poppins-semibold text-secondary lg:text-5xl text-3xl lg:mb-6 mb-2 mt-8">Sign in to</h1>
+          <h1 className="font-poppins-medium text-secondary lg:text-3xl text-xl lg:mb-12 mb-5">our user platform</h1>
+          <h1 className="font-poppins-regular text-sm sm:mb-10">If you don't have an account registered, <br /> You can register <a className="font-poppins-semibold text-blue-600 hover:underline" href="/register">here!</a></h1>
         </div>
 
-        <img className="w-7/12 mt-48" alt="login" src="/images/login.png" />
+        <img className="hidden xl:block w-7/12 mt-48" alt="login" src="/images/login.png" />
 
-        <div className="text-sm text-blue-darkBlue font-poppins-regular justify-end self-center -mt-44">
+        <div className="text-sm text-blue-darkBlue font-poppins-regular justify-end xl:self-center lg:mt-32 xl:-mt-44">
           <form>
-            <h1 className="text-black flex font-poppins-semibold text-2xl mb-9">Sign in</h1>
-            <input className="placeholder:text-blue-darkBlue focus:outline-none px-5 w-10/12 h-14 rounded-lg bg-blue-lightBlue mb-10"
+            <h1 className="text-black hidden lg:block font-poppins-semibold text-2xl mb-4">Sign in</h1>
+            <input className="placeholder:text-blue-darkBlue focus:outline-none px-5 lg:w-10/12 w-full h-14 rounded-lg bg-blue-lightBlue lg:mb-10 mb-6"
               placeholder="Email or username"
               type="email"
             />
-            <input className="placeholder:text-blue-darkBlue focus:outline-none px-5 w-10/12 h-14 rounded-lg bg-blue-lightBlue mb-11"
+            <input className="placeholder:text-blue-darkBlue focus:outline-none px-5 lg:w-10/12 w-full h-14 rounded-lg bg-blue-lightBlue lg:mb-11 mb-10"
               placeholder="Password"
               type="password"
             />
-            <button className="h-14 rounded-lg bg-blue-darkBlue mx-auto text-white w-10/12 shadow-xl" onClick={() => router.push("/home")}>Login</button>
+            <button className="h-14 rounded-lg bg-blue-darkBlue mx-auto text-white lg:w-10/12 w-full shadow-xl" onClick={() => router.push("/home")}>Login</button>
           </form>
+          <h1 className="text-gray-400 sm:my-8 mt-8 mb-4 text-center lg:w-10/12 w-full">or continue with</h1>
+          <div className="flex lg:w-10/12 w-full">
+            <div className="flex mx-auto">
+              <img className="h-10 mr-6" alt="icon" src="/images/apple.png" />
+              <img className="h-10" alt="icon" src="/images/google.png" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
