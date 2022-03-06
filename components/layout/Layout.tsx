c495d79f4c551c0076f46c3be26c909c607d061c
Felice <felicepng2010@gmail.com>
2022-03-06 11:04:06 +0800

    Fix drawer

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -44,25 +44,27 @@ function Layout(props: Props) {
   return (
     <div className={`font-poppins-medium text-gray-700 dark:text-gray-50 ${getTheme() === 'dark' && 'dark'}`}>
 
-      <Drawer placement="left" visible={width < 1280 && visibleDrawer} width="100%" closable={false}>
-        <div className="mt-9 flex items-center px-12 mb-7 gap-x-8">
-          <ImCross className="h-4 w-4" onClick={() => setVisibleDrawer(false)} />
-          <div className="font-poppins-semibold text-secondary text-xl">Menu</div>
+      <Drawer className={`${getTheme() === 'dark' && 'dark'}`} placement="left" visible={width < 1280 && visibleDrawer} width="100%" closable={false}>
+        <div className="dark:bg-secondary_dark h-screen">
+          <div className="dark:text-gray-50 pt-9 flex items-center px-12 mb-7 gap-x-8">
+            <ImCross className="h-4 w-4 cursor-pointer hover:text-blue-600 dark:hover:text-blue-200" onClick={() => setVisibleDrawer(false)} />
+            <div className="font-poppins-semibold text-secondary text-xl">Menu</div>
+          </div>
+          <LayoutItem title="Home" icon={<FiHome className="h-5 w-5" />} />
+          <LayoutItem title="Profile" icon={<CgUserList className="h-6 w-6" />} />
+          <LayoutItem title="Subscription" icon={<MdPayment className="h-5 w-5" />} />
+          <LayoutItem title="Settings" icon={<FiSettings className="h-5 w-5" />} />
+          <LayoutItem title="Feedback" icon={<MdOutlineFeedback className="h-5 w-5" />} />
+          <LayoutItem title="Help" icon={<IoMdHelp className="h-5 w-5" />} />
+          <hr className="mb-2 dark:opacity-30" />
+          <button onClick={() => {
+            saveUserId("");
+            router.push("/login");
+          }} className="w-full px-10 flex items-center py-4 text-base gap-5 text-gray-700 hover:text-blue-600 dark:text-gray-50 dark:hover:text-blue-200 hover:font-poppins-semibold">
+            <BiLogOut className="h-5 w-5" />
+            <div className="text-sm">Logout</div>
+          </button>
         </div>
-        <LayoutItem title="Home" icon={<FiHome className="h-5 w-5" />} />
-        <LayoutItem title="Profile" icon={<CgUserList className="h-6 w-6" />} />
-        <LayoutItem title="Subscription" icon={<MdPayment className="h-5 w-5" />} />
-        <LayoutItem title="Settings" icon={<FiSettings className="h-5 w-5" />} />
-        <LayoutItem title="Feedback" icon={<MdOutlineFeedback className="h-5 w-5" />} />
-        <LayoutItem title="Help" icon={<IoMdHelp className="h-5 w-5" />} />
-        <hr className="mb-2" />
-        <button onClick={() => {
-          saveUserId("");
-          router.push("/login");
-        }} className="w-full px-10 flex items-center py-4 text-base gap-5 text-gray-700 hover:text-blue-600 hover:font-poppins-semibold">
-          <BiLogOut className="h-5 w-5" />
-          <div className="text-sm">Logout</div>
-        </button>
       </Drawer>
 
       <div className="flex h-screen w-screen">
@@ -83,7 +85,7 @@ function Layout(props: Props) {
             <button onClick={() => {
               saveUserId("");
               router.push("/login");
-            }} className="w-full px-10 flex items-center py-4 text-base gap-5 text-gray-700 dark:text-gray-50 hover:text-blue-600 hover:font-poppins-semibold">
+            }} className="w-full px-10 flex items-center py-4 text-base gap-5 text-gray-700 dark:text-gray-50 dark:hover:text-blue-200 hover:text-blue-600 hover:font-poppins-semibold">
               <BiLogOut className="h-5 w-5" />
               <div className="text-sm">Logout</div>
             </button>
@@ -92,12 +94,12 @@ function Layout(props: Props) {
         <div className={`xl:ml-72 bg-primary_light dark:bg-primary_dark dark:text-gray-50 w-full ${visibleDrawer && "hidden xl:block"}`}>
           <div className="z-40 bg-primary_light dark:bg-primary_dark sticky top-0 px-12 pt-9 pb-5 justify-between flex items-center">
             <div className="flex items-center gap-x-8">
-              <TiThMenu className="xl:hidden h-5 w-5" onClick={() => setVisibleDrawer(true)} />
+              <TiThMenu className="xl:hidden h-5 w-5 cursor-pointer hover:text-blue-600 dark:hover:text-blue-200" onClick={() => setVisibleDrawer(true)} />
               <div className="font-poppins-semibold text-secondary text-xl xl:text-2xl">{title}</div>
             </div>
             <div className="flex items-center gap-x-7 xl:gap-x-9 xl:-mt-1 xl:mb-1">
               <DayNightToggle size={18} checked={getTheme() === "dark"} onChange={() => saveTheme(getTheme() === "dark" ? "light" : "dark")} />
-              <MdNotificationsNone className="w-6 h-6 xl:h-8 xl:w-8 text-gray-600 dark:text-gray-50 hover:text-blue-600 dark:hover:text-blue-2 cursor-pointer" />
+              <MdNotificationsNone className="w-6 h-6 xl:h-8 xl:w-8 text-gray-600 dark:text-gray-50 hover:text-blue-600 dark:hover:text-blue-200 cursor-pointer" />
               <FaUserCircle className="w-7 h-7 xl:w-8 xl:h-8 text-blue-600 hover:text-blue-700 dark:text-blue-200 dark:hover:text-blue-300 cursor-pointer" onClick={() => router.push("/profile")} />
             </div>
           </div>
