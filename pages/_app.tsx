1e7afd4f289c59dc46347dd0de05e7303bc6bbfb
Daryl Wong <youcanfinddaryl@gmail.com>
2022-03-23 18:48:12 +0800

    websocket connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,35 @@ import type { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client'
 import client from '../apollo-client'
 import AuthProvider from '../components/context/AppContext'
+// import WebSocket from 'ws'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const isBrowser = typeof window !== "undefined";
+  // const wsInstance = isBrowser ? new Websocket(...) : null;
+
+
+  useEffect(() => {
+    if (!isBrowser) {
+      return;
+    }
+
+    const ws = new WebSocket('ws://localhost:2999/1');
+
+    // ws.on('message', function message(data) {
+    //   console.log('received: %s', data);
+    // });
+    if (ws !== null) {
+
+      ws.onmessage = ((message:any) => {
+        console.log('received: ', message);
+
+      })
+    }
+
+  }, [])
+
   return (
     <ApolloProvider client={client}>
       <AuthProvider>
