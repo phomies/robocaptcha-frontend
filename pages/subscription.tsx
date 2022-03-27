855fc44916930f3cfb606261a9621fa99369d07a
Jerald <jeraldssw@gmail.com>
2022-03-27 18:52:55 +0800

    fix(subscription): replace API key with environment variable

--- pages/subscription.tsx
+++ pages/subscription.tsx
@@ -1,10 +1,10 @@
-import Layout from "../components/layout/Layout";
-import { MdDone } from "react-icons/md";
+import Layout from '../components/layout/Layout';
+import { MdDone } from 'react-icons/md';
 import StripeCheckout from 'react-stripe-checkout';
-import { useState, useEffect } from "react";
-import { useMutation, gql } from "@apollo/client";
-import { AppContext } from "../components/context/AppContext";
-import { useContext } from "react";
+import { useState, useEffect } from 'react';
+import { useMutation, gql } from '@apollo/client';
+import { AppContext } from '../components/context/AppContext';
+import { useContext } from 'react';
 
 const UPSERT_PAYMENT = gql`
   mutation upsertPayment($upsertPaymentInput: UpsertPaymentInput) {
@@ -12,81 +12,70 @@ const UPSERT_PAYMENT = gql`
       _id
     }
   }
-`
+`;
 
 function Subscription() {
   const { getFirebaseToken } = useContext(AppContext);
 
-  const [token, setToken] = useState<any>();
   const [amount, setAmount] = useState<number>(7);
-  const [dateStart, setDateStart] = useState<string>("");
-  const [dateEnd, setDateEnd] = useState<string>("");
-  const [transactionId, settransactionId] = useState<string>("");
-  const [plan, setPlan] = useState<string>("");
-
-  function handleToken(token: any) {
-    setToken(token);
-    
-    // upsertPayment();
-  }
+  const [token, setToken] = useState<any>();
+  const [dateStart, setDateStart] = useState<string>('');
+  const [dateEnd, setDateEnd] = useState<string>('');
+  const [transactionId, setTransactionId] = useState<string>('');
+  const [plan, setPlan] = useState<string>('');
 
   const [upsertPayment] = useMutation(UPSERT_PAYMENT, {
     context: {
       headers: {
-        'fbToken': getFirebaseToken()
-      }
+        fbToken: getFirebaseToken(),
+      },
     },
-    variables: {
-      upsertPaymentInput: {
-        amount: amount,
-        dateEnd: dateEnd,
-        dateStart: dateStart,
-        plan: plan,
-        transactionId: transactionId
-      }
-    }
-  })
+  });
 
-  useEffect(() => {
-    console.log(token);
-
-    settransactionId(token.id)
+  const handleToken = async (token: any) => {
     const current = new Date();
-    setDateStart(`${current.getFullYear()}/${current.getMonth() + 1}/${current.getDate()}`)
-    setDateEnd(`${current.getFullYear()}/${current.getMonth() + 2}/${current.getDate()}`)
-    setPlan("premium")
 
-    console.log(transactionId);
-    console.log(dateStart);
-    console.log(dateEnd);
-    console.log(plan);
-    console.log(amount);
-
-    upsertPayment();
-  }, [token])
+    await upsertPayment({
+      variables: {
+        upsertPaymentInput: {
+          amount: amount,
+          dateEnd: `${current.getFullYear()}/${current.getMonth() + 2}/${current.getDate()}`,
+          plan: "PREMIUM",
+          transactionId: token.id,
+        },
+      },
+    });
+  };
 
   return (
     <Layout>
       <div className="w-full px-12 pt-6 pb-12">
         <div className="bg-white dark:bg-secondary_dark shadow-lg rounded-lg p-6 mb-12 md:w-72 w-full mt-2">
-          <h1 className="font-poppins-regular text-gray-500 dark:text-gray-400">Next payment</h1>
-          <h1 className="font-poppins-semibold text-gray-700 dark:text-white">on 30 November 2020</h1>
+          <h1 className="font-poppins-regular text-gray-500 dark:text-gray-400">
+            Next payment
+          </h1>
+          <h1 className="font-poppins-semibold text-gray-700 dark:text-white">
+            on 30 November 2020
+          </h1>
 
           <StripeCheckout
             token={handleToken}
-            // stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE!}
-            stripeKey="pk_test_51KgSuUD4BheMu1ORMHlRgYf8asrY7t4llbnJZi7AeEcmFJ5e39bphoXX6yCkryTG4n50MUFcJWWTJGo8fL9554dI006JayrzYU"
+            stripeKey={process.env.NEXT_PUBLIC_STRIPE_API_KEY || ''}
             amount={amount * 100}
             name="Payment"
-          // description="survey credits"
-
           >
-            <button className="border border-blue-darkBlue text-blue-darkBlue bg-blue-lightBlue hover:bg-blue-100 dark:bg-blue-200 dark:hover:bg-blue-300  dark:text-gray-800 dark:border-0 w-full rounded-lg py-2  mt-5 shadow-sm">Make payment</button>
+            <button className="border border-blue-darkBlue text-blue-darkBlue bg-blue-lightBlue hover:bg-blue-100 dark:bg-blue-200 dark:hover:bg-blue-300  dark:text-gray-800 dark:border-0 w-full rounded-lg py-2  mt-5 shadow-sm">
+              Make payment
+            </button>
           </StripeCheckout>
         </div>
 
-        <h1 className="text-lg dark:text-white font-poppins-medium">Subscription models</h1>
-        <h1 className="font-poppins-regular text-sm mb-6 text-gray-500 dark:text-gray-400">Choose a plan that works best for you</h1>
+        <h1 className="text-lg dark:text-white font-poppins-medium">
+          Subscription models
+        </h1>
+        <h1 className="font-poppins-regular text-sm mb-6 text-gray-500 dark:text-gray-400">
+          Choose a plan that works best for you
+        </h1>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-8">
           <div className="bg-white dark:bg-secondary_dark shadow-lg rounded-lg p-6">
@@ -94,28 +83,40 @@ function Subscription() {
               <div className="bg-blue-darkBlue dark:bg-blue-200 h-12 w-12 rounded-xl mr-5"></div>
               <div className="my-auto">
                 <h1 className="text-base dark:text-white">Premium</h1>
-                <h1 className="font-poppins-regular text-sm text-gray-500 dark:text-gray-400">${amount} / month</h1>
+                <h1 className="font-poppins-regular text-sm text-gray-500 dark:text-gray-400">
+                  ${amount} / month
+                </h1>
               </div>
             </div>
 
             <hr className="my-5 dark:opacity-30" />
             <div className="flex mb-2">
               <MdDone className="grid my-auto mr-3" />
-              <h1 className="font-poppins-regular text-sm text-gray-500 dark:text-gray-300">Filter calls using customised blacklist and whitelist</h1>
+              <h1 className="font-poppins-regular text-sm text-gray-500 dark:text-gray-300">
+                Filter calls using customised blacklist and whitelist
+              </h1>
             </div>
             <div className="flex mb-2">
               <MdDone className="grid my-auto mr-3" />
-              <h1 className="font-poppins-regular text-sm text-gray-500 dark:text-gray-300">Automated verification of calls</h1>
+              <h1 className="font-poppins-regular text-sm text-gray-500 dark:text-gray-300">
+                Automated verification of calls
+              </h1>
             </div>
             <div className="flex mb-2">
               <MdDone className="grid my-auto mr-3" />
-              <h1 className="font-poppins-regular text-sm text-gray-500 dark:text-gray-300">Customisable verification level</h1>
+              <h1 className="font-poppins-regular text-sm text-gray-500 dark:text-gray-300">
+                Customisable verification level
+              </h1>
             </div>
             <div className="flex mb-2">
               <MdDone className="grid my-auto mr-3" />
-              <h1 className="font-poppins-regular text-sm text-gray-500 dark:text-gray-300">Call analysis</h1>
+              <h1 className="font-poppins-regular text-sm text-gray-500 dark:text-gray-300">
+                Call analysis
+              </h1>
             </div>
-            <button className="bg-blue-600 hover:bg-blue-700 dark:bg-blue-200 dark:hover:bg-blue-300 text-white dark:text-gray-800 w-full rounded-lg py-3 mt-5 shadow-sm">Choose plan</button>
+            <button className="bg-blue-600 hover:bg-blue-700 dark:bg-blue-200 dark:hover:bg-blue-300 text-white dark:text-gray-800 w-full rounded-lg py-3 mt-5 shadow-sm">
+              Choose plan
+            </button>
           </div>
           <div className="bg-white dark:bg-secondary_dark shadow-lg rounded-lg p-6 h-full flex flex-col justify-between">
             <div>
@@ -123,16 +124,22 @@ function Subscription() {
                 <div className="bg-blue-darkBlue dark:bg-blue-200 h-12 w-12 rounded-xl mr-5"></div>
                 <div className="my-auto">
                   <h1 className="text-base dark:text-white">Beginner</h1>
-                  <h1 className="font-poppins-regular text-sm text-gray-500 dark:text-gray-400">Free</h1>
+                  <h1 className="font-poppins-regular text-sm text-gray-500 dark:text-gray-400">
+                    Free
+                  </h1>
                 </div>
               </div>
               <hr className="my-5 dark:opacity-30" />
               <div className="flex mb-2">
                 <MdDone className="grid my-auto mr-3" />
-                <h1 className="font-poppins-regular text-sm text-gray-500 dark:text-gray-300">Filter calls using customised blacklist and whitelist</h1>
+                <h1 className="font-poppins-regular text-sm text-gray-500 dark:text-gray-300">
+                  Filter calls using customised blacklist and whitelist
+                </h1>
               </div>
             </div>
-            <button className="bg-blue-600 hover:bg-blue-700 dark:bg-blue-200 dark:hover:bg-blue-300 text-white dark:text-gray-800 w-full rounded-lg py-3  mt-5 shadow-sm">Choose plan</button>
+            <button className="bg-blue-600 hover:bg-blue-700 dark:bg-blue-200 dark:hover:bg-blue-300 text-white dark:text-gray-800 w-full rounded-lg py-3  mt-5 shadow-sm">
+              Choose plan
+            </button>
           </div>
         </div>
       </div>
