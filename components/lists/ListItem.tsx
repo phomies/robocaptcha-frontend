3be0f961bd7ab9eeb2651b9aaeda4e6d8801a00a
Felice <felicepng2010@gmail.com>
2022-03-28 09:41:27 +0800

    feat: Add Call list item

---
+++ components/lists/ListItem.tsx
@@ -0,0 +1,26 @@
+import { BiUserCircle } from "react-icons/bi";
+import { HiOutlineTrash } from "react-icons/hi";
+import { useMutation, gql } from "@apollo/client";
+
+interface Props {
+  isWhitelist: boolean
+  number: string
+}
+
+const ListItem = (props: Props) => {
+  const { isWhitelist, number } = props;
+
+  return (
+    <div className="dark:bg-secondary_dark text-xs md:text-sm py-4 px-6 flex justify-between">
+      <div className="flex items-center gap-x-3">
+        <BiUserCircle className="h-6 w-6 text-gray-500" />
+        {number.slice(0, 3) + " " + number.slice(3, 7) + " " + number.slice(7)}
+      </div>
+      <div className="flex items-center">
+        <HiOutlineTrash className="cursor-pointer w-5 h-5 text-red-400 hover:text-red-500" />
+      </div>
+    </div>
+  )
+}
+
+export default ListItem
--- pages/lists.tsx
+++ pages/lists.tsx
@@ -1,9 +1,9 @@
 import Layout from "../components/layout/Layout";
-import { BiUserCircle } from "react-icons/bi";
-import { HiOutlineTrash, HiPlusSm } from "react-icons/hi";
+import { HiPlusSm } from "react-icons/hi";
 import { useQuery, useMutation, gql } from "@apollo/client";
 import { AppContext } from "../components/context/AppContext";
 import { useContext } from "react";
+import ListItem from "../components/lists/ListItem";
 
 const GET_CONTACTS = gql`
   query getContacts {
@@ -33,8 +33,8 @@ function Lists() {
 
   return (
     <Layout>
-      <div className="w-full px-12 pt-6 pb-12 grid grid-cols-1 md:grid-cols-2 gap-x-10">
-        <div className="mt-7 py-1 bg-white dark:bg-secondary_dark shadow-lg rounded-lg w-full">
+      <div className="w-full px-12 pt-6 pb-12 flex flex-col md:flex-row gap-x-10">
+        <div className="mt-7 py-1 bg-white dark:bg-secondary_dark shadow-lg rounded-lg w-full h-full">
           <div className="py-4 px-7 text-sm md:text-base font-poppins-semibold flex items-center justify-between">
             Blacklist
             <HiPlusSm className="cursor-pointer h-6 w-6 text-gray-400 hover:text-blue-600 dark:hover:text-blue-200" />
@@ -44,23 +44,15 @@ function Lists() {
           </div>
           {
             data?.getUser.contacts.filter((item: any) => item.isBlacklisted).length > 0 ?
-              data.getUser.contacts.filter((item: any) => item.isBlacklisted).map((item: any) => {
-                <div className="dark:bg-secondary_dark text-xs md:text-sm py-4 px-6 flex justify-between">
-                  <div className="flex items-center gap-x-3">
-                    <BiUserCircle className="h-6 w-6 text-gray-500" />
-                    {item.number.slice(0, 3) + " " + item.number.slice(3, 7) + " " + item.number.slice(7)}
-                  </div>
-                  <div className="flex items-center">
-                    <HiOutlineTrash className="cursor-pointer w-5 h-5 text-red-400 hover:text-red-500" />
-                  </div>
-                </div>
-              }) :
+              data.getUser.contacts.filter((item: any) => item.isBlacklisted).map((item: any) =>
+                <ListItem key={item._id} isWhitelist={false} number={item.number as string} />
+              ) :
               <div className="font-poppins-regular text-gray-400 text-xs md:text-sm flex justify-center items-center w-full h-14 md:h-20">
                 No records found
               </div>
           }
         </div>
-        <div className="mt-7 py-1 bg-white dark:bg-secondary_dark shadow-lg rounded-lg w-full">
+        <div className="mt-7 py-1 bg-white dark:bg-secondary_dark shadow-lg rounded-lg w-full h-full">
           <div className="py-4 px-7 text-sm md:text-base font-poppins-semibold flex items-center justify-between">
             Whitelist
             <HiPlusSm className="cursor-pointer h-6 w-6 text-gray-400 hover:text-blue-600 dark:hover:text-blue-200" />
@@ -70,18 +62,10 @@ function Lists() {
           </div>
           {
             data?.getUser.contacts.filter((item: any) => item.isWhitelisted).length > 0 ?
-              data.getUser.contacts.filter((item: any) => item.isWhitelisted).map((item: any) => {
-                <div className="dark:bg-secondary_dark text-xs md:text-sm py-4 px-6 flex justify-between">
-                  <div className="flex items-center gap-x-3">
-                    <BiUserCircle className="h-6 w-6 text-gray-500" />
-                    {item.number.slice(0, 3) + " " + item.number.slice(3, 7) + " " + item.number.slice(7)}
-                  </div>
-                  <div className="flex items-center">
-                    <HiOutlineTrash className="cursor-pointer w-5 h-5 text-red-400 hover:text-red-500" />
-                  </div>
-                </div>
-              }) :
-              <div className="font-poppins-regular text-gray-400 text-xs md:text-sm flex justify-center items-center w-full h-14 md:h-20">
+              data.getUser.contacts.filter((item: any) => item.isWhitelisted).map((item: any) =>
+                <ListItem key={item._id} isWhitelist={true} number={item.number as string} />
+              ) :
+              <div className="font-poppins-regular text-gray-400 text-xs md:text-sm flex justify-center items-center w-full h-14">
                 No records found
               </div>
           }
