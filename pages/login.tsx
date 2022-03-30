9208aae2908e318aad121882e1df4a4aa63a53f6
Jerald <jeraldssw@gmail.com>
2022-03-30 22:56:06 +0800

    fix(auth): route after logging in with provider

--- pages/login.tsx
+++ pages/login.tsx
@@ -25,7 +25,6 @@ export default function Login() {
   const [contactNumber, setContactNumber] = useState<string>('');
   const [token, setToken] = useState<string>('');
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
-  const [isDisabled, setIsDisabled] = useState<boolean>(false);
   const [isEmailLogin, setIsEmailLogin] = useState<boolean>(true);
   const [isTokenLogin, setIsTokenLogin] = useState<boolean>(false);
   const firebaseAuth = getAuth(app);
@@ -50,7 +49,6 @@ export default function Login() {
           },
           firebaseAuth
         );
-
         setAppVerifier(verifier);
 
         // Recreate google login API
@@ -72,6 +70,7 @@ export default function Login() {
       {},
       (user: any) => {
         refreshGoogleToken(user);
+        router.push('/home');
       },
       (error: any) => {
         console.log(JSON.stringify(error));
@@ -92,6 +91,7 @@ export default function Login() {
   const handlePhoneLogin = async () => {
     try {
       await loginWithPhoneNumber(contactNumber);
+      router.push('/home');
     } catch (error) {
       console.log('Error with logging in with phone', error);
     }
@@ -100,6 +100,7 @@ export default function Login() {
   const handleTokenLogin = async () => {
     try {
       await validatePhoneToken(token);
+      router.push('/home');
     } catch (error) {
       console.log('Error with logging in with token', error);
     }
