59acba11e8b9330572a50679d8ca216ee48ce6fa
Jerald <jeraldssw@gmail.com>
2022-04-04 01:19:43 +0800

    fix(wss): amend env var

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -105,7 +105,7 @@ function Layout(props: Props) {
       console.log("checking connection");
       
       if (ws === null || ws.readyState !== ws.OPEN) {
-        ws = new WebSocket("wss://" + process.env.notification.robocaptcha.xyz + '/' + userId);
+        ws = new WebSocket("wss://" + process.env.NEXT_PUBLIC_WEBSOCKET_URL + '/' + userId);
         ws.onmessage = ((message: any) => {
           console.log('received: ', message);
           notify(JSON.parse(message.data));
