a004b5956bf72734acde5ecb5116dd5b2ac02b26
Jerald <jeraldssw@gmail.com>
2022-04-05 23:13:13 +0800

    feat: expose server side env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    env: {
+        NEXT_PUBLIC_STRIPE_API_KEY: process.env.NEXT_PUBLIC_STRIPE_API_KEY,
+        NEXT_PUBLIC_NOTIFICATION_URL: process.env.NEXT_PUBLIC_NOTIFICATION_URL
+    }
 }
 
 module.exports = nextConfig
